af8a30186ce1be50435b72acca92c3bbbd8e04d7
白济明 <1085455474@qq.com>
2020-03-24 20:29:54 +0800

    add requirements

--- autoUpdate.py
+++ autoUpdate.py
@@ -3,10 +3,11 @@
 import requests
 import time
 
-USERNAME = "2018213639"
-PASSWORD = "1085455474"
-FORMDATA1 = 'ismoved=0&jhfjrq=&jhfjjtgj=&jhfjhbcc=&sfxk=0&xkqq=&uid=40448&date='
-FORMDATA2 = '&tw=3&sfcxtz=0&sfyyjc=0&jcjgqr=0&jcjg=&sfjcbh=0&sfcxzysx=0&qksm=&remark=&address=%E5%90%89%E6%9E%97%E7%9C%81%E9%95%BF%E6%98%A5%E5%B8%82%E6%9C%9D%E9%98%B3%E5%8C%BA%E6%A1%82%E6%9E%97%E8%A1%97%E9%81%93%E5%90%8C%E5%85%89%E8%B7%AF5-9-3%E9%97%A8&area=%E5%90%89%E6%9E%97%E7%9C%81+%E9%95%BF%E6%98%A5%E5%B8%82+%E6%9C%9D%E9%98%B3%E5%8C%BA&province=%E5%90%89%E6%9E%97%E7%9C%81&city=%E9%95%BF%E6%98%A5%E5%B8%82&geo_api_info=%7B%22type%22%3A%22complete%22%2C%22position%22%3A%7B%22P%22%3A43.870060492622%2C%22O%22%3A125.32097195095503%2C%22lng%22%3A125.320972%2C%22lat%22%3A43.87006%7D%2C%22location_type%22%3A%22html5%22%2C%22message%22%3A%22Get+ipLocation+failed.Get+geolocation+success.Convert+Success.Get+address+success.%22%2C%22accuracy%22%3A65%2C%22isConverted%22%3Atrue%2C%22status%22%3A1%2C%22addressComponent%22%3A%7B%22citycode%22%3A%220431%22%2C%22adcode%22%3A%22220104%22%2C%22businessAreas%22%3A%5B%7B%22name%22%3A%22%E6%A1%82%E6%9E%97%E8%B7%AF%22%2C%22id%22%3A%22220104%22%2C%22location%22%3A%7B%22P%22%3A43.867444%2C%22O%22%3A125.31685500000003%2C%22lng%22%3A125.316855%2C%22lat%22%3A43.867444%7D%7D%2C%7B%22name%22%3A%22%E6%B0%B8%E6%98%8C%22%2C%22id%22%3A%22220104%22%2C%22location%22%3A%7B%22P%22%3A43.877416%2C%22O%22%3A125.316822%2C%22lng%22%3A125.316822%2C%22lat%22%3A43.877416%7D%7D%2C%7B%22name%22%3A%22%E5%8D%97%E5%B2%AD%22%2C%22id%22%3A%22220102%22%2C%22location%22%3A%7B%22P%22%3A43.859268%2C%22O%22%3A125.33823899999999%2C%22lng%22%3A125.338239%2C%22lat%22%3A43.859268%7D%7D%5D%2C%22neighborhoodType%22%3A%22%22%2C%22neighborhood%22%3A%22%22%2C%22building%22%3A%22%22%2C%22buildingType%22%3A%22%22%2C%22street%22%3A%22%E5%90%8C%E5%85%89%E8%B7%AF%22%2C%22streetNumber%22%3A%225-9-3%E9%97%A8%22%2C%22province%22%3A%22%E5%90%89%E6%9E%97%E7%9C%81%22%2C%22city%22%3A%22%E9%95%BF%E6%98%A5%E5%B8%82%22%2C%22district%22%3A%22%E6%9C%9D%E9%98%B3%E5%8C%BA%22%2C%22township%22%3A%22%E6%A1%82%E6%9E%97%E8%A1%97%E9%81%93%22%7D%2C%22formattedAddress%22%3A%22%E5%90%89%E6%9E%97%E7%9C%81%E9%95%BF%E6%98%A5%E5%B8%82%E6%9C%9D%E9%98%B3%E5%8C%BA%E6%A1%82%E6%9E%97%E8%A1%97%E9%81%93%E5%90%8C%E5%85%89%E8%B7%AF5-9-3%E9%97%A8%22%2C%22roads%22%3A%5B%5D%2C%22crosses%22%3A%5B%5D%2C%22pois%22%3A%5B%5D%2C%22info%22%3A%22SUCCESS%22%7D&created=1585020295&sfzx=0&sfjcwhry=0&sfcyglq=0&gllx=&glksrq=&jcbhlx=&jcbhrq=&sftjwh=0&sftjhb=0&fxyy=&bztcyy=&fjsj=0&sfjchbry=0&sfjcqz=&jcqzrq=&jcwhryfs=&jchbryfs=&xjzd=&szgj=&sfsfbh=0&jhfjsftjwh=0&jhfjsftjhb=0&szsqsfybl=0&sfygtjzzfj=0&gtjzzfjsj=&sfsqhzjkk=0&sqhzjkkys=&created_uid=22739&id=1475714&gwszdd=&sfyqjzgc=&jrsfqzys=&jrsfqzfy='
+#请填写此处字段
+USERNAME = "??"
+PASSWORD = "??"
+FORMDATA1 = '??' #单引号
+FORMDATA2 = '??' #单引号
 
 def print_msg(msg,level=1):
     if level is 1:
